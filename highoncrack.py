c460273dc278e555b56506cad93de22663b27bc9
ski-sketch <mustafamoamen980@gmail.com>
2024-06-27 02:10:52 +0400

    hello

--- highoncrack.py
+++ highoncrack.py
@@ -1,8 +1,23 @@
-rows = int(input("how many rows do you want?"))
-columns = int(input("how many columns do you want?"))
-symbol = input("what symbol do you want?")
+import random
 
-for i in range(rows):
-    for n in range(columns):
-        print(symbol, end=" ")
-    print()
+choices = ["rock", "paper", "scissors"]
+
+x = random.choice(choices)
+userchoice = input("please enter rock, paper, or scissors: ")
+while userchoice == "":
+    print("you didn't enter anything")
+    userchoice = input("please enter rock, paper, or scissors: ")
+if userchoice == x:
+    print ("it is a tie")
+elif userchoice =="rock" and x == "scissors":
+    print("you win")
+elif userchoice =="rock" and x =="paper":
+    print("you lose")
+elif userchoice =="paper" and x == "rock":
+    print("you win")
+elif userchoice =="paper" and x == "scissors":
+    print("you lose")
+elif userchoice =="scissors" and x == "rock":
+    print("you lose")
+elif userchoice =="scissors" and x == "paper":
+    print("you win")
